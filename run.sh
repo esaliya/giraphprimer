2875a6d8e3decaac0ff8dff476ac944e44569cd2
Saliya Ekanayake <esaliya@hswlogin1.cm.cluster>
2017-02-11 12:59:40 -0500

    adding run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,17 @@
 #rm -rf /home/esaliya/sali/projects/vt/giraph/gout.path
 #java -cp /home/esaliya/sali/git/github/esaliya/java/giraphprimer/target/giraphprimer-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.giraphprimer.withmaster.customdata.MultilinearMain 5 3 5 10 /home/esaliya/sali/projects/vt/giraph/path_graph_simple.txt /home/esaliya/sali/projects/vt/giraph/gout.path 1 local false
 
+#$HADOOP_HOME/bin/hdfs dfs -rm -r -f /gout.path
+#path graph 1
+#$HADOOP_HOME/bin/hadoop jar /home/esaliya/sali/git/github/esaliya/java/giraphprimer/target/giraphprimer-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.giraphprimer.withmaster.customdata.MultilinearMain 5 3 5 10 /gin.path/path_graph_simple.txt /gout.path 4 hsw003:5431 true
+
 $HADOOP_HOME/bin/hdfs dfs -rm -r -f /gout.path
-$HADOOP_HOME/bin/hadoop jar /home/esaliya/sali/git/github/esaliya/java/giraphprimer/target/giraphprimer-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.giraphprimer.withmaster.customdata.MultilinearMain 5 3 5 10 /gin.path/path_graph_simple.txt /gout.path 4 hsw003:5431 true
+#path graph 2
+#$HADOOP_HOME/bin/hadoop jar /home/esaliya/sali/git/github/esaliya/java/giraphprimer/target/giraphprimer-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.giraphprimer.withmaster.customdata.MultilinearMain 5 3 3 10 /gin.path.2/path_graph_simple_2.txt /gout.path 4 hsw003:5431 true
+
+#syn.g.1
+#$HADOOP_HOME/bin/hadoop jar /home/esaliya/sali/git/github/esaliya/java/giraphprimer/target/giraphprimer-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.giraphprimer.withmaster.customdata.MultilinearMain 100 8 7 10 /syn.g.1/er-k8-0-n100-p20.txt /gout.path 4 hsw003:5431 true
+#syn.g.5k
+#$HADOOP_HOME/bin/hadoop jar /home/esaliya/sali/git/github/esaliya/java/giraphprimer/target/giraphprimer-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.giraphprimer.withmaster.customdata.MultilinearMain 5000 8 7 10 /syn.g.5k/er-k8-0-n5000-p20.txt /gout.path 16 hsw003:5431 true
+#syn.g.1k
+$HADOOP_HOME/bin/hadoop jar /home/esaliya/sali/git/github/esaliya/java/giraphprimer/target/giraphprimer-1.0-SNAPSHOT-jar-with-dependencies.jar org.saliya.giraphprimer.withmaster.customdata.MultilinearMain 1000 8 7 10 /syn.g.1k/er-k8-0-n1000-p20.txt /gout.path 100 hsw003:5431 true
