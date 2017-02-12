a13238d22168c1a8774d3bbdec86a327c13cdd9f
Saliya Ekanayake <esaliya@gmail.com>
2017-02-12 02:41:04 -0500

    with fake comm

--- src/main/java/org/saliya/giraphprimer/withmaster/customdata/MultilinearWorker.java
+++ src/main/java/org/saliya/giraphprimer/withmaster/customdata/MultilinearWorker.java
@@ -47,78 +47,78 @@ public class MultilinearWorker extends BasicComputation<
         int localSS = (int)ss % MultilinearWorkerContext.workerSteps;
         // The external loop number that goes from 0 to twoRaisedToK (excluding)
         int iter = (int)ss / MultilinearWorkerContext.workerSteps;
-//        if (ss == 0){
-//            // Set array in vertex data
-//            // We'll use the last element, i.e. k+1 idx, to keep track of the vertex id
-//            vData.vertexRow = new int[mwc.k+2];
-//            vData.vertexRowLength = vData.vertexRow.length;
-//            vData.randomWeightToComputeCircuitSum = getRandomWeightToComputeCircuitSum(
-//                    new Random(mwc.randomSeed), fieldSize, vData.vertexId);
-//            long[] nums = this.<LongArrayWritable>getBroadcast(MultilinearMaster.MULTILINEAR_RANDOM_NUMS).getData();
-//            vData.uniqueRandomSeed = nums[vData.vertexId];
-//        }
-//        // In the original code I started from 2 and went till k (including)
-//        // we start localSS from zero, which is for initialization
-//        // then real work begins with localSS=1, which should give I=2, hence I=localSS+1
-//        int I = localSS+1;
-//        if (localSS == 0){
-//            vData.random = new Random(vData.uniqueRandomSeed);
-//            IntStream.range(0, vData.vertexRowLength).forEach(x -> vData.vertexRow[x] = 0);
-//            // Set the last element to the vertex id, so when messages are collected
-//            // we can find which vertex it belongs to
-//            vData.vertexRow[vData.vertexRowLength-1] = vData.vertexId;
-//            int color = vData.vertexColor;
-//            int dotProduct = mwc.randomAssignment[color] & iter;
-//            vData.vertexRow[1] = (Integer.bitCount(dotProduct) % 2 == 1) ? 0 : 1;
-//
-//        } else {
-//            // business logic only if localSS != 0
-//
-//            // handy test code
-//           /* if (vertex.getId().get() == 1) {
-//                System.out.println("Vertex: " + vertex.getId().get() + " i: " + I + " ss: " + ss + " localSS: " + localSS + " iter: " + iter);
-//            }*/
-//
-//            long t = System.currentTimeMillis();
-//            TreeMap<Integer, int[]> messagesSortedByVertexId = new TreeMap<>();
-//            for (IntArrayWritable message: messages){
-//                int [] data = message.getData();
-//                // TODO: debug - let's fake to reduce comm.
-////                messagesSortedByVertexId.put(data[vData.vertexRowLength-1], data);
-//                messagesSortedByVertexId.put(data[data.length - 1], data);
-//            }
-//            sortTime += System.currentTimeMillis() - t;
-//
-//            t = System.currentTimeMillis();
-//            Set<Integer> neighborsInAscendingOrder = messagesSortedByVertexId.keySet();
-//            for (int j = 1; j < I; j++) {
-//                for (int neighbor: neighborsInAscendingOrder) {
-//                    int weight = vData.random.nextInt(fieldSize);
-//                    // TODO: debug - let's fake to reduce comm. The side effect is we don't have this, so let's make it constant
-////                    int product = gf.ffMultiply(vData.vertexRow[j], messagesSortedByVertexId.get(neighbor)[I - j]);
-//                    int product = gf.ffMultiply(vData.vertexRow[j], 1);
-//                    product = gf.ffMultiply(weight, product);
-//                    vData.vertexRow[I] = gf.add(vData.vertexRow[I], product);
-//                }
-//            }
-//            computeTime += System.currentTimeMillis() - t;
-//        }
-//
-//        if (localSS != (MultilinearWorkerContext.workerSteps -1)){
-//            long t = System.currentTimeMillis();
-//            // TODO: debug - let's fake to reduce this
-////            IntArrayWritable message = new IntArrayWritable(vData.vertexRow);
-//            IntArrayWritable message = new IntArrayWritable(new int[]{vData.vertexId});
-//            for (Edge<IntWritable, NullWritable> edge : vertex.getEdges()) {
-//                sendMessage(edge.getTargetVertexId(), message);
-//            }
-//            sendTime += System.currentTimeMillis() - t;
-//
-//        } else {
-//            aggregate(MultilinearMaster.MULTILINEAR_CIRCUIT_SUM,
-//                    new IntWritable(gf.ffMultiply(
-//                            vData.randomWeightToComputeCircuitSum, vData.vertexRow[mwc.k])));
-//        }
+        if (ss == 0){
+            // Set array in vertex data
+            // We'll use the last element, i.e. k+1 idx, to keep track of the vertex id
+            vData.vertexRow = new int[mwc.k+2];
+            vData.vertexRowLength = vData.vertexRow.length;
+            vData.randomWeightToComputeCircuitSum = getRandomWeightToComputeCircuitSum(
+                    new Random(mwc.randomSeed), fieldSize, vData.vertexId);
+            long[] nums = this.<LongArrayWritable>getBroadcast(MultilinearMaster.MULTILINEAR_RANDOM_NUMS).getData();
+            vData.uniqueRandomSeed = nums[vData.vertexId];
+        }
+        // In the original code I started from 2 and went till k (including)
+        // we start localSS from zero, which is for initialization
+        // then real work begins with localSS=1, which should give I=2, hence I=localSS+1
+        int I = localSS+1;
+        if (localSS == 0){
+            vData.random = new Random(vData.uniqueRandomSeed);
+            IntStream.range(0, vData.vertexRowLength).forEach(x -> vData.vertexRow[x] = 0);
+            // Set the last element to the vertex id, so when messages are collected
+            // we can find which vertex it belongs to
+            vData.vertexRow[vData.vertexRowLength-1] = vData.vertexId;
+            int color = vData.vertexColor;
+            int dotProduct = mwc.randomAssignment[color] & iter;
+            vData.vertexRow[1] = (Integer.bitCount(dotProduct) % 2 == 1) ? 0 : 1;
+
+        } else {
+            // business logic only if localSS != 0
+
+            // handy test code
+           /* if (vertex.getId().get() == 1) {
+                System.out.println("Vertex: " + vertex.getId().get() + " i: " + I + " ss: " + ss + " localSS: " + localSS + " iter: " + iter);
+            }*/
+
+            long t = System.currentTimeMillis();
+            TreeMap<Integer, int[]> messagesSortedByVertexId = new TreeMap<>();
+            for (IntArrayWritable message: messages){
+                int [] data = message.getData();
+                // TODO: debug - let's fake to reduce comm.
+//                messagesSortedByVertexId.put(data[vData.vertexRowLength-1], data);
+                messagesSortedByVertexId.put(data[data.length - 1], data);
+            }
+            sortTime += System.currentTimeMillis() - t;
+
+            t = System.currentTimeMillis();
+            Set<Integer> neighborsInAscendingOrder = messagesSortedByVertexId.keySet();
+            for (int j = 1; j < I; j++) {
+                for (int neighbor: neighborsInAscendingOrder) {
+                    int weight = vData.random.nextInt(fieldSize);
+                    // TODO: debug - let's fake to reduce comm. The side effect is we don't have this, so let's make it constant
+//                    int product = gf.ffMultiply(vData.vertexRow[j], messagesSortedByVertexId.get(neighbor)[I - j]);
+                    int product = gf.ffMultiply(vData.vertexRow[j], 1);
+                    product = gf.ffMultiply(weight, product);
+                    vData.vertexRow[I] = gf.add(vData.vertexRow[I], product);
+                }
+            }
+            computeTime += System.currentTimeMillis() - t;
+        }
+
+        if (localSS != (MultilinearWorkerContext.workerSteps -1)){
+            long t = System.currentTimeMillis();
+            // TODO: debug - let's fake to reduce this
+//            IntArrayWritable message = new IntArrayWritable(vData.vertexRow);
+            IntArrayWritable message = new IntArrayWritable(new int[]{vData.vertexId});
+            for (Edge<IntWritable, NullWritable> edge : vertex.getEdges()) {
+                sendMessage(edge.getTargetVertexId(), message);
+            }
+            sendTime += System.currentTimeMillis() - t;
+
+        } else {
+            aggregate(MultilinearMaster.MULTILINEAR_CIRCUIT_SUM,
+                    new IntWritable(gf.ffMultiply(
+                            vData.randomWeightToComputeCircuitSum, vData.vertexRow[mwc.k])));
+        }
 
 
         if (iter == (MultilinearWorkerContext.twoRaisedToK -1) && localSS == (MultilinearWorkerContext.workerSteps -1)){
